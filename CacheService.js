e028d8f62f39f4799b06bc91e5cfea03f26e0032
imm <imm@imms-iMac.local>
2022-02-02 11:58:02 +0100

    qa review

---
+++ CacheService.js
@@ -0,0 +1,26 @@
+const NodeCache = require( "node-cache" );
+const cache = new NodeCache();
+
+async function set(key, value, ttl = null) {
+    if (ttl) {
+        return cache.set(key, value, ttl);
+    }
+
+    return cache.set(key, value);
+}
+
+async function get(key) {
+    return cache.get(key);
+}
+
+async function del(key) {
+    return cache.del(key);
+}
+
+module.exports = {
+    set: set,
+    get: get,
+    del: del
+}
+© 2022 GitHub, Inc.
+Terms
